fa0c911d25c6a2ec8daa8223c9d3be496d9c29a5
AlexeyKondrukevich <kondr08@mail.ru>
2023-08-16 08:23:33 +0400

    Change settings

--- news/news/settings.py
+++ news/news/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", 0))
-
+# DEBUG = bool(os.environ.get("DEBUG", 0))
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 
