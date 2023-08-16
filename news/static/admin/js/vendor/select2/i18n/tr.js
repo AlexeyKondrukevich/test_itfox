5c885b14dfedcbb2b06dbfc26694a247364b7ea5
AlexeyKondrukevich <kondr08@mail.ru>
2023-08-16 08:43:03 +0400

    Change settings static

---
+++ news/static/admin/js/vendor/select2/i18n/tr.js
@@ -0,0 +1,1 @@
+/Users/aleksejkondrukevic/Dev/test_itfox/venv/lib/python3.9/site-packages/django/contrib/admin/static/admin/js/vendor/select2/i18n/tr.js
---
+++ news/static/drf-yasg/swagger-ui-dist/swagger-ui-es-bundle.js
@@ -0,0 +1,1 @@
+/Users/aleksejkondrukevic/Dev/test_itfox/venv/lib/python3.9/site-packages/drf_yasg/static/drf-yasg/swagger-ui-dist/swagger-ui-es-bundle.js
